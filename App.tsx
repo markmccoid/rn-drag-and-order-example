7b04a0538ea78c12db0d3e7a09f14893d3ee7243
Mark <markmccoid@gmail.com>
2021-11-14 00:02:29 -0500

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,21 +1,188 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { MotiView } from "@motify/components";
+import DragDropEntry, {
+  DragItem,
+  sortArray,
+  TScrollFunctions,
+} from "@markmccoid/react-native-drag-and-order";
+import { useStore, ItemType } from "./store";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 export default function App() {
+  // prevNumberOfItems used in useEffect (commented out by default)
+  const prevNumberOfItems = React.useRef(0);
+  const [newItem, setNewItem] = React.useState("");
+  const items = useStore((state) => state.itemList);
+  const addItem = useStore((state) => state.addItem);
+  const removeItemById = useStore((state) => state.removeItemById);
+  const updateItemList = useStore((state) => state.updateItemList);
+
+  const [scrollFunctions, setScrollFunctions] = React.useState<TScrollFunctions>();
+
+  //* The useEffect below is used to scroll to the end or start
+  //* on adds and deletes of items.  Without this, the scroll will not
+  //* move on adds/deletes of items
+  React.useEffect(() => {
+    if (scrollFunctions) {
+      // scrollFunctions.scrollToEnd();
+      const numberOfItems = items.length;
+      if (numberOfItems > prevNumberOfItems.current) {
+        console.log("Scrolling To End >");
+        scrollFunctions.scrollToEnd();
+      } else if (numberOfItems < prevNumberOfItems.current) {
+        //Item has been removed
+        // probably you do not want to scroll the list as this would be confusing
+        // -----
+        // scrollFunctions.scrollToStart();
+      }
+    }
+    prevNumberOfItems.current = items.length;
+  }, [items.length]);
+  //************************************************* */
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>D & D Grocery List </Text>
+      <View style={styles.separator} />
+      <DragDropEntry
+        scrollStyles={{ width: "100%", height: "30%", borderWidth: 1, borderColor: "#aaa" }}
+        updatePositions={(positions) =>
+          updateItemList(sortArray<ItemType>(positions, items, "pos"))
+        }
+        getScrollFunctions={(functionObj) => setScrollFunctions(functionObj)}
+        itemHeight={50}
+        handlePosition="left"
+        // handle={AltHandle}
+        enableDragIndicator={true}
+        dragIndicatorConfig={{
+          translateXDistance: 100,
+          indicatorBackgroundColor: "#abcabcaa",
+          indicatorBorderRadius: 20,
+        }}
+        // dragIndicator={testDragIndicator}
+      >
+        {items.map((item, idx) => {
+          return (
+            <DragItem
+              key={item.id}
+              name={item.name}
+              id={item.id}
+              itemHeight={50}
+              onRemoveItem={() => {
+                removeItemById(item.id);
+              }}
+            />
+          );
+        })}
+      </DragDropEntry>
+      <View style={{ flexDirection: "column", alignItems: "center" }}>
+        <TextInput
+          style={{
+            width: 200,
+            // height: 30,
+            borderWidth: 1,
+            paddingHorizontal: 10,
+            paddingVertical: 5,
+            margin: 5,
+            marginTop: 10,
+            fontSize: 20,
+          }}
+          placeholder="Enter New item"
+          value={newItem}
+          onChangeText={setNewItem}
+          // onSubmitEditing={(e) => addItem(e.nativeEvent.text)}
+        />
+        <TouchableOpacity
+          style={{
+            padding: 10,
+            borderWidth: 1,
+            margin: 5,
+            backgroundColor: "#0084fa",
+            borderRadius: 10,
+          }}
+          onPress={() => {
+            addItem(newItem);
+            setNewItem("");
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 19 }}>Add New Item</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={{
+            padding: 10,
+            borderWidth: 1,
+            margin: 5,
+            backgroundColor: "#0084fa",
+
+            borderRadius: 10,
+          }}
+          onPress={() => {
+            scrollFunctions?.scrollToStart();
+          }}
+        >
+          <Text style={{ color: "white", fontSize: 19 }}>Scroll To Start</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View
+        style={{ width: "70%", height: "40%", borderWidth: 1, padding: 5, marginBottom: 10 }}
+      >
+        <ScrollView
+          stickyHeaderIndices={[0]}
+          style={{ marginHorizontal: 10, paddingHorizontal: 10 }}
+        >
+          <View key={0} style={{ borderBottomColor: "#777", borderBottomWidth: 1 }}>
+            <View
+              style={{
+                flexDirection: "row",
+                width: "100%",
+                justifyContent: "space-between",
+              }}
+            >
+              <Text style={{ fontSize: 16 }}>Item</Text>
+              <Text style={{ fontSize: 16 }}>Pos</Text>
+            </View>
+          </View>
+          {items.map((item) => (
+            <View
+              key={item.id}
+              style={{ flexDirection: "row", justifyContent: "space-between" }}
+            >
+              <Text style={{ fontSize: 16 }}>{`${item.name}`}</Text>
+              <Text style={{ fontSize: 16 }}>{`${item.pos}`}</Text>
+            </View>
+          ))}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "flex-start",
+    borderWidth: 1,
+    // borderColor: "red",
+  },
+  title: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  separator: {
+    marginVertical: 5,
+    height: 1,
+    width: "80%",
   },
 });
---
+++ store.ts
@@ -0,0 +1,82 @@
+import create, { UseStore } from "zustand";
+import sortBy from "lodash/sortBy";
+import { Positions, updatePositionArrayField } from "@markmccoid/react-native-drag-and-order";
+
+export type ItemType = {
+  id: number | string;
+  name: string;
+  pos: number;
+};
+
+type StoreState = {
+  itemList: ItemType[];
+  addItem: (name: string) => void;
+  removeItem: () => void;
+  removeItemById: (id) => void;
+  updateItemList: (newList: ItemType[] | undefined) => void;
+  updatePositions: (positions: Positions) => void;
+  updatePositions2: (positions: Positions) => void;
+};
+
+export const useStore = create<StoreState>((set) => ({
+  itemList: [
+    { id: "a", name: "Coconut Milk", pos: 0 },
+    { id: "b", name: "Lettuce", pos: 1 },
+    { id: "c", name: "Walnuts", pos: 2 },
+    { id: "d", name: "Chips", pos: 3 },
+    { id: "e", name: "Ice Cream", pos: 4 },
+    { id: "f", name: "Carrots", pos: 5 },
+    { id: "g", name: "Onions", pos: 6 },
+    { id: "h", name: "Cheese", pos: 7 },
+    { id: "i", name: "Frozen Dinners", pos: 8 },
+    { id: "j", name: "Yogurt", pos: 9 },
+    { id: "k", name: "Kombucha", pos: 10 },
+    { id: "l", name: "Lemons", pos: 11 },
+    { id: "m", name: "Bread", pos: 12 },
+  ],
+  addItem: (name) =>
+    set((state) => ({
+      itemList: [
+        ...state.itemList,
+        { id: state.itemList.length + 1, name, pos: state.itemList.length },
+      ],
+    })),
+  removeItem: () =>
+    set((state) => ({ itemList: [...state.itemList.slice(0, state.itemList.length - 1)] })),
+  removeItemById: (id) =>
+    set((state) => ({
+      itemList: updatePositionArrayField(
+        [...state.itemList.filter((item) => item.id !== id)],
+        "pos"
+      ),
+    })),
+  updateItemList: (newList: ItemType[] | undefined) =>
+    set((state) => ({ itemList: newList || [] })),
+  updatePositions: (positions: Positions) =>
+    set((state) => {
+      const newItems: ItemType[] = [];
+      for (const [key, value] of Object.entries(positions)) {
+        state.itemList.forEach((el) => {
+          if (el.id === parseInt(key)) {
+            el.pos = positions[key];
+            newItems.push(el);
+          }
+        });
+      }
+      return { itemList: newItems };
+    }),
+  updatePositions2: (positions: Positions) =>
+    set((state) => {
+      const newItems: ItemType[] = [];
+      for (const [key, value] of Object.entries(positions)) {
+        state.itemList.forEach((el) => {
+          if (el.id === parseInt(key)) {
+            el.pos = positions[key];
+            newItems.push(el);
+          }
+        });
+      }
+
+      return { itemList: sortBy(newItems, ["pos"]) };
+    }),
+}));
